827759c84fe4854b1325e0e1613270bf45d260ef
castillocarlosr <castillocarlosr@gmail.com>
2019-01-09 19:35:38 -0800

    done for the night

--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -2,86 +2,186 @@
 
 namespace Lab02_UnitTesting
 {
-    class Program
+    public class Program
     {
+        //example starter balance
         public static decimal balance = 5000m;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Code401 ATM");
             Console.WriteLine("\n");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+
+        // bool to give options for ATM
+        public static bool MainMenu()
+        {
             Console.WriteLine("Choose an option you would like to make:");
             Console.WriteLine("1) View Balance:");
             Console.WriteLine("2) Withdraw Money:");
             Console.WriteLine("3) Deposit Money:");
-            Console.WriteLine("");
             Console.WriteLine("4) Exit ATM:");
 
             string inputChoice = Console.ReadLine();
 
             try
             {
-                if(inputChoice == "1")
+                if (inputChoice == "1")
                 {
                     ViewBalance(balance);
+                    return true;
                 }
-                else if(inputChoice == "2")
+                else if (inputChoice == "2")
                 {
                     WithdrawMoney();
+                    return true;
                 }
-                else if(inputChoice == "3")
+                else if (inputChoice == "3")
                 {
                     DepositMoney();
+                    return true;
                 }
-                else if(inputChoice == "4")
+                else if (inputChoice == "4")
                 {
-                    ExitATM();
+                    return false;
                 }
             }
-            catch (Exception )
+            catch (Exception all)
             {
-
+                Console.WriteLine("Looks like you didn't type a valid option");
+                Console.WriteLine(all.Message);
+                Console.ReadLine();
                 throw;
             }
+            return true;
         }
 
+        //Just to view current balamce
         public static decimal ViewBalance(decimal insideBalance)
         {
-            decimal currentBalance = balance;
-            Console.WriteLine($"${balance}");
-            Console.ReadLine();
-            
-            return balance;
+            try
+            {
+                decimal currentBalance = balance;
+                Console.WriteLine($"${balance}");
+                Console.WriteLine("Press ENTER to return to Main ATM Menu.");
+                Console.ReadLine();
+                return balance;
+            }
+            catch (Exception all)
+            {
+                Console.WriteLine("Looks you typed text instead of numbers.");
+                Console.WriteLine(all.Message);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using our ATM :)");
+                Console.ReadLine();
+            }
+
 
         }
 
+        //Console display to ask user how much to withdraw and reord user input
         public static decimal WithdrawMoney()
         {
-            decimal currentBalance = balance;
-            Console.Write("How much money do you want to WITHDRAW: $");
-            string inputWithdraw = Console.ReadLine();
-            decimal inputWithdrawDecimal = decimal.Parse(inputWithdraw);
+            try
+            {
+                decimal currentBalance = balance;
+                Console.Write("How much money do you want to WITHDRAW: $");
+                string inputWithdraw = Console.ReadLine();
+                decimal inputWithdrawDecimal = decimal.Parse(inputWithdraw);
+                WithdrawInput(inputWithdrawDecimal);
+            }
+            catch (Exception all)
+            {
+                Console.WriteLine("ATM only handles numbers.  Not letters.");
+                Console.WriteLine(all.Message);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using our ATM :)");
+                Console.ReadLine();
+            }
 
-            balance = currentBalance - inputWithdrawDecimal;
-            //AppDomain();
             return balance;
         }
 
+        //This is where that user input goes into to subtract from balance.  Negative number will ask user to try again.
+        public static decimal WithdrawInput(decimal insideValue)
+        {
+            decimal currentBalance = balance;
+            if (insideValue < 0)
+            {
+                Console.WriteLine("Nice try.  You can't negative a withdraw.");
+                Console.WriteLine("");
+            }
+            else if ((currentBalance - insideValue) > 0)
+            {
+                balance = currentBalance - insideValue;
+                Console.WriteLine($"You have withdrawn ${insideValue}");
+                Console.WriteLine("");
+                return balance;
+            }
+            else
+            {
+                Console.WriteLine("Sorry.  You do NOT have that much money!");
+                Console.WriteLine($"Your current balance is ${currentBalance}");
+                Console.WriteLine("");
+                return currentBalance;
+            }
+            return 0;
+        }
+
+        //This is to ask user for how much to deposit and record user input
         public static decimal DepositMoney()
         {
-            decimal currentBalance = balance;
-            Console.Write("How much money do you want to DEPOSIT: $");
-            string inputDeposit = Console.ReadLine();
-            decimal inputDepositDecimal = decimal.Parse(inputDeposit);
+            try
+            {
+                decimal currentBalance = balance;
+                Console.Write("How much money do you want to DEPOSIT: $");
+                string inputDeposit = Console.ReadLine();
+                decimal inputDepositDecimal = decimal.Parse(inputDeposit);
+                DepositMoneyInput(inputDepositDecimal);
+            }
+            catch (Exception all)
+            {
+                Console.WriteLine("Looks you typed text instead of numbers.");
+                Console.WriteLine(all.Message);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using our ATM :)");
+                Console.ReadLine();
+            }
 
-            balance = currentBalance + inputDepositDecimal;
             return balance;
         }
 
-        public static bool ExitATM()
+        //This is where the user input adds to the current balance.  Again negative number will ask user to try again.
+        public static decimal DepositMoneyInput(decimal insideValue)
         {
-            return false;
+            decimal currentBalance = balance;
+            if (insideValue < 0)
+            {
+                Console.WriteLine("Nice try.  You can't negative a deposit.");
+                Console.WriteLine("");
+                return balance;
+            }
+            else
+            {
+                balance = currentBalance + insideValue;
+                Console.WriteLine($"You have deposited ${insideValue}");
+                Console.WriteLine("");
+                return balance;
+            }
         }
-
     }
 }
--- README.md
+++ README.md
@@ -12,30 +12,30 @@ It will allow you to "view Balance", "deposit money", and "withdraw money".
 There will try-catch-finally-throw methods implemented into the program.
 
 ### How to run the program:
-This program requires visual studio.
+This program requires visual studio 2017.
 Run the program to start the console application.
 This will be under "Program.cs".
 
 You may look at the source code to ensure nothing evil is in there.
 
-* The console will ask five different questions that's fun and interesting about me.
-* The console will display if your answer and if you guessed correctly.
-* If you typed in invalid guess an Exception error will display showing you what the error was.
-** example: How old am I?  If you type "birds" the console will tell you that you did not type in a valid number.
-* When all 5 questions are answered, the console will display the number of correct guesses you made.
+* The console will welcome you to the ATM
+* The console will display what options you want.  
+*- Deposit, Withdraw, View Balance, or Exit.
+* Your starting balance is $5000
+* The ATM will not let you cheat by typing a negative number.
+* Don't spend all your money at once.
 
-This assignment took about 4 hours to complete.  1 hour the night before and 3 hours during morning lab time.
+This assignment took about 5 hours to complete.  1 hour the night before and 4 hours during morning lab time.
+I kept forgetting things like the throw.
+I couldn't get the test to pass, even though the code looked correct.
+
+Got the code to pass eventually.  Just Re-Typed the same test.
 
 Below are screen shots of what this should look like.
 ###Screen Shot 
-#### Intro Screen Shot
-![](ScreenShots/CaptureMain.PNG?raw=true)
+![](assets/Capture1.PNG?raw=true)
+![](assets/Capture2.PNG?raw=true)
 
-#### Valid answers Screen Shot
-![](ScreenShots/CaptureCorrect.PNG?raw=true)
 
-#### Invalid answers Screen Shot
-![](ScreenShots/CaptureCatchTry.PNG?raw=true)
-
-Lab started on January 08, 2019 by Carlos R. Castillo
+Lab started on January 09, 2019 by Carlos R. Castillo
 Thanks for stopping by and stay classy Seattle.
